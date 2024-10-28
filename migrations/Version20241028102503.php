d12e16367904347e9046418f39383ea85ae0ba13
Vincent Ravanel <vravanel@gmail.com>
2024-10-28 12:03:18 +0100

    add tests for Agence entity validation : - Implement getEntity for default Agence values, -Create assertHasErrors() to validate entities, Add tests for valid and invalid agency names.

---
+++ migrations/Version20241028102503.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241028102503 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE agence CHANGE name name VARCHAR(50) NOT NULL, CHANGE adress adress VARCHAR(100) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE agence CHANGE name name VARCHAR(150) NOT NULL, CHANGE adress adress VARCHAR(255) NOT NULL');
+    }
+}
